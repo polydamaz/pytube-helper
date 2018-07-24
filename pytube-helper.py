f44181ce37780819dc2c73c93a6718790b9f6424
pdz <polydamaz@gmail.com>
2018-07-24 22:59:37 +0900

    1st complete

--- pytube-helper.py
+++ pytube-helper.py
@@ -1,98 +1,79 @@
 # -*- coding: utf-8 -*-
 import os, sys, time
 import argparse
-
+from urllib import urlopen
+from pytube import YouTube
 # import re
 # from urllib2 import urlopen
 
-import sys
-
-from urllib import urlopen
-from pytube import YouTube
-
-class PytHelper(YouTube):
+class PytHelper(
+    YouTube):
     'Sub Class'
-
-    def __init__(self, arguments=[], defer_prefetch_init=False):
-        self.dfaultPlayListUrl = 'https://www.youtube.com/watch?v=2NR1FOoSPio&list=PLzMlWTOO8JNB6wjaz1WI2dZrtZ9-T6cSj'
-        self.url = arguments[0] if len(arguments) > 1 else self.dfaultPlayListUrl
-
-        YouTube.__init__(self, self.url, defer_prefetch_init)
-
-        self.playListUrlPrefix = ''
+    def __init__(self, url, downloadDir, defer_prefetch_init=False):
+        YouTube.__init__(self, url, defer_prefetch_init)
+        
+        self.url = url
+        self.downloadDir = downloadDir
         self.playList = []
-        self.args = arguments
-        self.startTime = ''
-
+        self.playListURL = ''
+        
         self.main()
 
     def main(self):
-        if self.__hasList() is True:
-            self.__extractPlayList()
-            print('This is playlist!!!', self.playList)
-        else:
-            print('This is Not playlist!!!')
-        
-        #self.startTime = time.time()
 
-        """ 질의 """
-        parser = argparse.ArgumentParser(usage='%(prog)s [-h] [-p PLAYLISTURL] [-d DESTINATION]')
-        parser.add_argument('-p', '--playlisturl', help='url of the playlist to be downloaded', default=self.dfaultPlayListUrl, metavar='')
-        parser.add_argument('-d', '--destination', help='path of directory to save videos to', default=os.path.curdir, metavar='')
-        args = parser.parse_args()
-
-        if os.path.exists(args.destination):
-                directory_contents = [f.split('.mp4',1)[0] for f in os.listdir(args.destination) if f.endswith('.mp4')]
+        if os.path.exists(self.downloadDir):
+            directory_contents = [f.split('.mp4',1)[0] for f in os.listdir(self.downloadDir) if f.endswith('.mp4')]
         else:
             print('Destination directory does not exist')
             sys.exit(1)
 
-        confirmation = raw_input("You are about to download {0} videos to {1}\nWould you like to continue? Y/n ".format(len(self.playList), os.path.abspath(args.destination)))
+        confirmation = raw_input("You are about to download {0} videos to {1}\nWould you like to continue? Y/n ".format(len(self.playList), os.path.abspath(self.downloadDir)))
         
-        if confirmation.lower() in ['y', '']:
-            for u in self.playList:
-                yt = YouTube(u)
-                vid = yt.streams.filter(mime_type='video/mp4').order_by('resolution').last()
-
-		if vid.default_filename in directory_contents:
-			print('Skipping {}'.format(vid.default_filename))
-			continue
-		else:
-			print('Downloading {}'.format(vid.default_filename))
-			vid.download(args.destination)
-			print('Done')
+        if confirmation.lower() in ['n']:
+            print 'GoodBye'
+            sys.exit()
         
+        self.playListURL = self.getList(self.url)
 
-    # @staticmethod
-    # def print_dot(bytes_received, file_size, start):
-    #     if time.time() - start > 1.0:
-    #         sys.stdout.write('.')
-    #         sys.stdout.flush()
-    #         start = time.time()
+        if self.playListURL:
+            self.playList = self.extractPlayList(self.playListURL)
+        else: 
+            self.playList.append(self.url)
 
-
-    # @staticmethod
-    def __hasList(self):
-        """ str find 와 split """
-        urlParam = self.url[self.url.find('?')+1:]
-        for el in urlParam.split('&'):
+        for u in self.playList:
+            yt = YouTube(u)
+            vid = yt.streams.filter(mime_type='video/mp4').order_by('resolution').last()
+            if vid.default_filename in directory_contents:
+                print('Skipping {}'.format(vid.default_filename))
+                continue
+            else:
+                print('Downloading {}'.format(vid.default_filename))
+                vid.download(self.downloadDir) 
+                print('Done')
+		
+        
+    @staticmethod
+    def getList(url):
+        qstr = url[url.find('?')+1:]
+        for el in qstr.split('&'):
             if 'list' in el.split('='):
-                self.playListUrlPrefix = 'https://www.youtube.com/playlist?{0}'.format(el)
-                return True
-
-        return False
-
-    # @staticmethod
-    def __extractPlayList(self):
-        for line in urlopen(self.playListUrlPrefix).readlines():
+                return 'https://www.youtube.com/playlist?{}'.format(el)
+        
+    
+    @staticmethod
+    def extractPlayList(url):
+        playList = []
+        for line in urlopen(url).readlines():
             tag = line.decode('utf8')
             if 'pl-video-title-link' in tag:
-                self.playList.append('https://www.youtube.com' + tag.split('href="',1)[1].split('" ',1)[0])
+                playList.append('https://www.youtube.com' + tag.split('href="',1)[1].split('" ',1)[0])
 
-        print self.playList
-
+        return playList
 
 if __name__ == "__main__" :
-    arguments = sys.argv if len(sys.argv) > 1 else []
+    parser = argparse.ArgumentParser(usage='%(prog)s [-h] [-p PLAYLISTURL] [-d DESTINATION]')
+    parser.add_argument('-p', '--playlisturl', help='url of the playlist to be downloaded', default='https://www.youtube.com/watch?v=aWrZ1QDs1tQ', metavar='')
+    parser.add_argument('-d', '--destination', help='path of directory to save videos to', default=os.path.curdir, metavar='')
+    args = parser.parse_args()
 
-    PytHelper(arguments)
+    PytHelper(args.playlisturl, args.destination)
