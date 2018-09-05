aa3dc168dc2bfaf63bbb4e37bac0964fa8f2e93f
pdz <polydamaz@gmail.com>
2018-09-05 16:51:59 +0900

    chacnge file

---
+++ util.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def textLog(title, context):
+    f = open(title, 'w')
+    f.write(str(context))
+    f.close()
+
+if __name__ == '__main__':
+    # textLog('test', 'awdawwwwwwwwwwwwwwwww')
+    pass
